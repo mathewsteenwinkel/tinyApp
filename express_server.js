983b77cbd6ac01ae0b100a3e14c8ea20d0dc4399
Noidea22 <mathewsteenwinkel@gmail.com>
2023-02-21 21:35:00 -0700

    added loggin and cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,15 +1,19 @@
 
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 
 };
+
+//for cookies and username
 
 
 
@@ -31,13 +35,17 @@ function generateRandomString(len) {
   return text;
 }
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"],
+  }
   res.render("urls_index", templateVars);
 });
 
@@ -51,12 +59,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars)
 });
 
 app.get("/urls/:id", (req, res) => {
-  console.log(req)
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  //console.log(req)
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   }
   res.render("urls_show", templateVars)
 });
 
@@ -67,7 +82,15 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL]= longURL;
   //console.log(longURL)
   res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
+
 });
+
+app.post("/urls/login", (req, res) => {
+
+  res.cookie ('username',req.body.username)
+  res.redirect("/urls");
+});
+
 
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
@@ -89,6 +112,8 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[shortUrl]
   res.redirect("/urls");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
